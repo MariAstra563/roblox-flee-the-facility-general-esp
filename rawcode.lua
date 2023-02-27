345d22b2013bb1861ec731a6af93dce408acafc3
AstroCOL563 <124411319+AstroCOL563@users.noreply.github.com>
2023-02-26 21:18:55 -0500

    RAW CODE FOR HACK

---
+++ rawcode.lua
@@ -0,0 +1,45 @@
+while true do
+	game.Workspace[game.Players.LocalPlayer.Name].Humanoid.WalkSpeed = 32
+    for i, o in pairs(game.Workspace:GetChildren()) do
+        if o:FindFirstChild("Humanoid") then
+            if o.Name ~= game.Players.LocalPlayer.Name then
+                if o:FindFirstChild("Hammer") then
+                    if o:FindFirstChild("HighlightA") then
+                        o.HighlightA:Destroy()
+                    end
+                    local hl = Instance.new("Highlight")
+                    hl.Name = "HighlightA"
+                    hl.Parent = o
+                    hl.OutlineTransparency = 1
+                    hl.FillTransparency = 0.25
+                    hl.FillColor = Color3.new(1, 0, 0)
+                else
+                    if o:FindFirstChild("HighlightA") then
+                        o.HighlightA:Destroy()
+                    end
+                    local hl = Instance.new("Highlight")
+                    hl.Name = "HighlightA"
+                    hl.Parent = o
+                    hl.OutlineTransparency = 1
+                    hl.FillTransparency = 0.25
+                    hl.FillColor = Color3.new(0, 1, 0)
+                end
+            end
+        end
+        if o:FindFirstChild("ComputerTable") then
+            for i2, o2 in pairs(o:GetChildren()) do
+                if o2.Name == "ComputerTable" then
+                    if not o2:FindFirstChild("HighlightA") then
+                        local hl = Instance.new("Highlight")
+                        hl.Name = "HighlightA"
+                        hl.Parent = o2
+                        hl.OutlineTransparency = 1
+                        hl.FillTransparency = 0.25
+                        hl.FillColor = Color3.new(0, 0, 1)
+                    end
+                end
+            end
+        end
+    end
+    wait()
+end
