0e7b87b10280350d8d22d9321bff92f1b4f09607
fori <124411319+MariAstra563@users.noreply.github.com>
2024-09-25 21:30:47 -0400

    fixed laggy and bad code

--- rawcode.lua
+++ rawcode.lua
@@ -1,51 +1,43 @@
-while true do
-    for i, o in pairs(game.Workspace:GetChildren()) do
-        if o:FindFirstChild("Humanoid") then
-            if o.Name ~= game.Players.LocalPlayer.Name then
-                if o:FindFirstChild("Hammer") then
-                    if o:FindFirstChild("HighlightA") then
-                        o.HighlightA:Destroy()
-                    end
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "HighlightA"
-                    hl.Parent = o
-                    hl.OutlineTransparency = 1
-                    hl.FillTransparency = 0.25
-                    hl.FillColor = Color3.new(1, 0, 0)
-                else
-                    if o:FindFirstChild("HighlightA") then
-                        o.HighlightA:Destroy()
-                    end
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "HighlightA"
-                    hl.Parent = o
-                    hl.OutlineTransparency = 1
-                    hl.FillTransparency = 0.25
-                    hl.FillColor = Color3.new(0, 1, 0)
-                end
-            end
+local user_name = game.Players.LocalPlayer.Name
+
+local rs = game:GetService("RunService")
+
+local function make_highlight(parent, color)
+    if parent:FindFirstChild("HighlightA") then
+        parent.HighlightA.FillColor = color
+        
+        return
+    end
+
+    local hl = Instance.new("Highlight")
+
+    hl.Name = "HighlightA"
+    hl.Parent = parent
+    hl.OutlineTransparency = 1
+    hl.FillTransparency = 0.25
+    hl.FillColor = color
+end
+
+rs.RenderStepped:Connect(function()
+    for _, plr in pairs(game.Players:GetPlayers()) do
+        local char = plr.Character
+
+        if not char then continue end
+        if not char:FindFirstChild("Humanoid") then continue end
+        if char.Name == user_name then continue end
+
+        if char:FindFirstChild("Hammer") then
+            make_highlight(char, Color3.new(1, 0, 0))
+        else
+            make_highlight(char, Color3.new(0, 1, 0))
         end
-        for i2, o2 in pairs(o:GetChildren()) do
-            if o2.Name == "ComputerTable" then
-                if not o2:FindFirstChild("HighlightA") then
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "HighlightA"
-                    hl.Parent = o2
-                    hl.OutlineTransparency = 1
-                     hl.FillTransparency = 0.25
-                    hl.FillColor = Color3.new(0, 0, 1)
-                end
-            elseif o2.Name == "ExitDoor" then
-                if not o2:FindFirstChild("HighlightA") then
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "HighlightA"
-                    hl.Parent = o2
-                    hl.OutlineTransparency = 1
-                     hl.FillTransparency = 0.25
-                    hl.FillColor = Color3.new(1, 1, 0)
-                end
+
+        for i, o in pairs(game.Workspace:GetDescendants()) do
+            if o.Name == "ComputerTable" then
+                make_highlight(o, Color3.new(0, 1, 1))
+            elseif o.Name == "ExitDoor" then
+                make_highlight(o, Color3.new(1, 1, 0))
             end
         end
     end
-    wait()
-end
+end)
